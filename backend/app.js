025da29414c8313d589e7fadea6bbe561ba500ff
Yoni190 <yfisuhu@gmail.com>
2026-04-13 18:27:40 +0300

    Use cors

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 require('dotenv').config()
 const routes = require('./routes/index')
+const cors = require('cors')
 
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 
 app.use(routes.authRoute)
