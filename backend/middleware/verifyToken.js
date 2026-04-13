bd53e3fc52801030c2b545a7923ccfa694143431
Yoni190 <yfisuhu@gmail.com>
2026-04-13 16:26:30 +0300

    Verify token in the middleware function

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,11 +1,19 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
 const verifyToken = (req, res, next) => {
     const bearerHeader = req.headers['authorization']
 
     if(typeof bearerHeader !== 'undefined') {
         const bearerToken = bearerHeader.split(' ')[1]
 
-        req.token = bearerToken
-        next()
+        jwt.verify(bearerToken, process.env.JWT_SECRET, (err, authData) => {
+            if(err) {
+                return res.sendStatus(403)
+            }
+            req.token = bearerToken
+            next()
+        })
     } else {
         res.sendStatus(403)
     }
