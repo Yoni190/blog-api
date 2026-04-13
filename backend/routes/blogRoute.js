bc4a8eaaf2efbb0d5ee6db9babfcd338deeff890
Yoni190 <yfisuhu@gmail.com>
2026-04-13 16:15:16 +0300

    Create blogRoute

---
+++ backend/routes/blogRoute.js
@@ -0,0 +1,7 @@
+const { Router } = require('express')
+const blogController = require('../controllers/blogController')
+
+const router = Router()
+
+
+module.exports = router
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,7 @@
 const authRoute = require('./authRoute')
+const blogRoute = require('./blogRoute')
 
 module.exports = {
-    authRoute
+    authRoute,
+    blogRoute
 }
