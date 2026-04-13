5c134af02e3b46d2377e2347816c40a7f4ad6b60
Yoni190 <yfisuhu@gmail.com>
2026-04-13 17:56:18 +0300

    Create function to create comments

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -1,7 +1,7 @@
 const { prisma } = require('../lib/prisma')
 
 const index = async (req, res) => {
-    const blogId = parseInt(req.params.id)
+    const blogId = parseInt(req.query.blogId)
 
     const comments = await prisma.comment.findMany({
         where: { blogId }
@@ -10,6 +10,23 @@ const index = async (req, res) => {
     res.json({ comments })
 }
 
+const create = async (req, res) => {
+    const user = req.authData.user
+    const blogId = parseInt(req.query.blogId)
+    const { text } = req.body
+
+    await prisma.comment.create({
+        data: {
+            text,
+            blogId,
+            userId: user.id
+        }
+    })
+
+    res.json({ message: 'Comment Created Successfully!' })
+}
+
 module.exports = {
-    index
+    index,
+    create
 }
