9f195f28343e332bdbd463efd9a21406004c0537
Yoni190 <yfisuhu@gmail.com>
2026-04-13 17:13:04 +0300

    Create route to update blog publish status

--- backend/routes/blogRoute.js
+++ backend/routes/blogRoute.js
@@ -9,5 +9,6 @@ router.get('/', blogController.index)
 router.post('/', verifyToken, authorizeRole('AUTHOR'), blogController.create)
 router.put('/:id', verifyToken, authorizeRole('AUTHOR'), blogController.edit)
 router.delete('/:id', verifyToken, authorizeRole('AUTHOR'), blogController.destroy)
+router.patch('/:id', verifyToken, authorizeRole('AUTHOR'), blogController.updatePublishStatus)
 
 module.exports = router
