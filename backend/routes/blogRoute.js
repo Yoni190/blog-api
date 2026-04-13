f89397a697857d3a7a2e9ab23a94bb2ba242b80e
Yoni190 <yfisuhu@gmail.com>
2026-04-13 16:46:31 +0300

    Create route to create blogs

--- backend/routes/blogRoute.js
+++ backend/routes/blogRoute.js
@@ -5,6 +5,7 @@ const verifyToken = require('../middleware/verifyToken')
 const router = Router()
 
 router.get('/', blogController.index)
+router.post('/', verifyToken, blogController.create)
 
 
 module.exports = router
