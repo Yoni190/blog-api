45ea7dc494358d0498d070487d4a655254ad59a8
Yoni190 <yfisuhu@gmail.com>
2026-04-13 18:27:56 +0300

    Make index function display all blogs

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,11 +1,8 @@
 const { prisma } = require('../lib/prisma')
 
 const index = async (req, res) => {
-    const userId = req.authData.user.id
 
-    const blogs = await prisma.blog.findMany({
-        where: { authorId: userId }
-    })
+    const blogs = await prisma.blog.findMany()
 
     res.json({ blogs })
 }
