48feec2f13d962e3fb5d68ab4af4b38377fc5a29
Yoni190 <yfisuhu@gmail.com>
2026-04-13 17:56:02 +0300

    Create route to create comments

--- backend/routes/commentRoute.js
+++ backend/routes/commentRoute.js
@@ -6,5 +6,6 @@ const verifyToken = require('../middleware/verifyToken')
 const authorizeRole = require('../middleware/authorizeRole')
 
 router.get('/', commentController.index)
+router.post('/', verifyToken, authorizeRole('USER'), commentController.create)
 
 module.exports = router
