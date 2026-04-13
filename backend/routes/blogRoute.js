75b584e87701e212819d00b01b890b5879f458ae
Yoni190 <yfisuhu@gmail.com>
2026-04-13 16:36:14 +0300

    Create route to get blogs

--- backend/routes/blogRoute.js
+++ backend/routes/blogRoute.js
@@ -1,7 +1,10 @@
 const { Router } = require('express')
 const blogController = require('../controllers/blogController')
+const verifyToken = require('../middleware/verifyToken')
 
 const router = Router()
+
+router.get('/', verifyToken, blogController.index)
 
 
 module.exports = router
