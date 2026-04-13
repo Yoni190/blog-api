93e9c63f1168b162439554100e5680e0ab637d94
Yoni190 <yfisuhu@gmail.com>
2026-04-13 16:59:18 +0300

    Add authorizeRoute middleware to blogRoutes

--- backend/routes/blogRoute.js
+++ backend/routes/blogRoute.js
@@ -1,11 +1,12 @@
 const { Router } = require('express')
 const blogController = require('../controllers/blogController')
 const verifyToken = require('../middleware/verifyToken')
+const authorizeRole = require('../middleware/authorizeRole')
 
 const router = Router()
 
 router.get('/', blogController.index)
-router.post('/', verifyToken, blogController.create)
-router.put('/:id', verifyToken, blogController.edit)
+router.post('/', verifyToken, authorizeRole('AUTHOR'), blogController.create)
+router.put('/:id', verifyToken, authorizeRole('AUTHOR'), blogController.edit)
 
 module.exports = router
