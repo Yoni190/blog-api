3a5eae13417b3643ab4fe5a3503d0b94c925e7d6
Yoni190 <yfisuhu@gmail.com>
2026-04-15 18:42:46 +0300

    Display blog details

--- users-frontend/src/pages/BlogDetails.jsx
+++ users-frontend/src/pages/BlogDetails.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
 
 const BlogDetails = () => {
+  const { id } = useParams()
+  const [blog, setBlog] = useState({})
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/blogs/${id}`)
+      .then((res) => res.json())
+      .then((res) => setBlog(res.blog))
+      .catch((err) => console.error(err))
+  }, [])
+
+  console.log(blog)
+  
   return (
-    <div>BlogDetails</div>
+    <div>
+      <h1>{blog.title}</h1>
+      <h2>{blog.author?.username}</h2>
+      <p>{blog.text}</p>
+    </div>
   )
 }
 
