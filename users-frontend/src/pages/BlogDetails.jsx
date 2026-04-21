0f92c3879ae514942857820cf9e463827c61b97f
Yoni190 <yfisuhu@gmail.com>
2026-04-21 17:35:28 +0300

    Create function to post a comment

--- users-frontend/src/pages/BlogDetails.jsx
+++ users-frontend/src/pages/BlogDetails.jsx
@@ -6,6 +6,7 @@ const BlogDetails = () => {
   const [blog, setBlog] = useState({})
   const token = localStorage.getItem('jwt_token')
   const [loggedIn, setLoggedIn] = useState(false)
+  const [comment, setComment] = useState('')
 
   useEffect(() => {
     fetch(`http://localhost:3000/blogs/${id}`)
@@ -34,7 +35,29 @@ const BlogDetails = () => {
         
       }, [token])
 
-  console.log(blog)
+      const submitComment = async (e) => {
+        e.preventDefault()
+
+        try {
+          const blogId = await blog.id
+          console.log(blogId)
+          const res = await fetch(`http://localhost:3000/comments`, {
+            method: 'POST',
+            headers: {
+              'Authorization': `Bearer ${token}`,
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              text: comment,
+              blogId
+            })
+          })
+
+          console.log(res)
+        } catch (error) {
+          console.error(error)
+        }
+      }
   
   return (
     <div>
@@ -44,9 +67,9 @@ const BlogDetails = () => {
 
       {loggedIn && (
         <div>
-          <form action="">
+          <form onSubmit={submitComment}>
             <h2>Add a Comment</h2>
-            <textarea name="comment" id="comment"></textarea>
+            <textarea name="text" id="text" value={comment} onChange={(e) => setComment(e.target.value)}></textarea>
             <button type='submit'>Post Comment</button>
           </form>
         </div>
