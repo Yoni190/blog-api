cd9acb951c7be5d497c6367cf14f6d13920e0853
Yoni190 <yfisuhu@gmail.com>
2026-04-21 17:03:30 +0300

    Create route to authenticate users

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,9 +1,11 @@
 const { Router } = require('express')
 const authController = require('../controllers/authController')
+const verifyToken = require('../middleware/verifyToken')
 
 const router = Router()
 
 router.post('/register', authController.register)
 router.post('/login', authController.login)
+router.get('/me', verifyToken, authController.me)
 
 module.exports = router
