5a20f30225bd500720162eae8fb908a6a32d1052
Yoni190 <yfisuhu@gmail.com>
2026-04-21 18:21:19 +0300

    Create login function

--- author-frontend/src/pages/Login.jsx
+++ author-frontend/src/pages/Login.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleLogin = async (e) => {
+        e.preventDefault()
+
+        try {
+            console.log(username)
+            const res = await fetch('http://localhost:3000/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                    isAuthor: true
+                })
+            })
+
+            const data = await res.json()
+            localStorage.setItem('jwt_token', data.token)
+        } catch (error) {
+            console.error(error)
+        }
+        
+    }
   return (
-    <div>Login</div>
+    <div>
+        <form onSubmit={handleLogin}>
+            <input type="username" name="username" id="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+            <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+
+            <button type='submit'>Log In</button>
+        </form>
+    </div>
   )
 }
 
