b3ec66d333826309d00a8afd1e7eadd5f6837417
Yoni190 <yfisuhu@gmail.com>
2026-04-21 17:20:10 +0300

    Show comment form if user is authenticated

--- users-frontend/src/pages/BlogDetails.jsx
+++ users-frontend/src/pages/BlogDetails.jsx
@@ -4,6 +4,8 @@ import { useParams } from 'react-router'
 const BlogDetails = () => {
   const { id } = useParams()
   const [blog, setBlog] = useState({})
+  const token = localStorage.getItem('jwt_token')
+  const [loggedIn, setLoggedIn] = useState(false)
 
   useEffect(() => {
     fetch(`http://localhost:3000/blogs/${id}`)
@@ -11,6 +13,26 @@ const BlogDetails = () => {
       .then((res) => setBlog(res.blog))
       .catch((err) => console.error(err))
   }, [])
+
+  useEffect(() => {
+  
+        const authenticateUser = async () => {
+          try {
+            const res = await fetch('http://localhost:3000/me', {
+              headers: {
+                'Authorization': `Bearer ${token}`
+              }
+            })
+  
+            setLoggedIn(res.ok)
+          } catch (error) {
+            console.error(error)
+          }
+        }
+  
+        authenticateUser()
+        
+      }, [token])
 
   console.log(blog)
   
@@ -20,13 +42,16 @@ const BlogDetails = () => {
       <h2>{blog.author?.username}</h2>
       <p>{blog.text}</p>
 
-      <div>
-        <form action="">
-          <h2>Add a Comment</h2>
-          <textarea name="comment" id="comment"></textarea>
-          <button type='submit'>Post Comment</button>
-        </form>
-      </div>
+      {loggedIn && (
+        <div>
+          <form action="">
+            <h2>Add a Comment</h2>
+            <textarea name="comment" id="comment"></textarea>
+            <button type='submit'>Post Comment</button>
+          </form>
+        </div>
+      )}
+      
     </div>
   )
 }
