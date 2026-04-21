72932f559c3f3a062f7bbd8efbc43eed8d733e2e
Yoni190 <yfisuhu@gmail.com>
2026-04-21 18:30:04 +0300

    Navigate to login if user is not authenticated

--- author-frontend/src/pages/Home.jsx
+++ author-frontend/src/pages/Home.jsx
@@ -1,6 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router'
+
 
 const Home = () => {
+    const token = localStorage.getItem('jwt_token')
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if(!token) {
+            navigate('/')
+            return
+        }
+
+        const authenticate = async () => {
+            try {
+                const res = await fetch('http://localhost:3000/me', {
+                    headers: {
+                        'authorization': `Bearer ${token}`
+                    }
+                })
+
+                console.log(res)
+
+                if(!res.ok) {
+                    navigate('/')
+                }
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        authenticate()
+      
+    }, [token])
+    
   return (
     <div>Home</div>
   )
