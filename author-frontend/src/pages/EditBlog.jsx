5c988f4c95bf9948c877edaf87994486daf21dbd
Yoni190 <yfisuhu@gmail.com>
2026-04-22 16:22:02 +0300

    Check that the user is authenticated in edit blog page

--- author-frontend/src/pages/EditBlog.jsx
+++ author-frontend/src/pages/EditBlog.jsx
@@ -1,6 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router'
+
 
 const EditBlog = ({ id }) => {
+    const token = localStorage.getItem('jwt_token')
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+            if(!token) {
+                navigate('/')
+                return
+            }
+    
+            const authenticate = async () => {
+                try {
+                    const res = await fetch('http://localhost:3000/me', {
+                        headers: {
+                            'authorization': `Bearer ${token}`
+                        }
+                    })
+    
+                    if(!res.ok) {
+                        navigate('/')
+                    }
+                } catch (error) {
+                    console.error(error)
+                }
+            }
+    
+            authenticate()
+          
+        }, [token])
   return (
     <div>EditBlog</div>
   )
